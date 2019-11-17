29b866f3e81320575a88020722974f6a58b7605a
Samuel Lando <samuel.lando@mail.concordia.ca>
2019-11-16 21:27:45 -0500

    Fix keys in data

--- backend/api/index.php
+++ backend/api/index.php
@@ -13,36 +13,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$CREATE = 0;
-
-// CREATE the tables.
-if ($CREATE) {
-    $sql = "CREATE TABLE locations (
-        id VARCHAR(225) PRIMARY KEY,
-        lng FLOAT(53),
-        lat FLOAT(53),
-        name VARCHAR(255),
-        avg INT(255),
-        dsc VARCHAR(500)
-    )";
-    if ($conn->query($sql) === TRUE) {
-       echo "Created locations table";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
-    $sql = "CREATE TABLE mcgilltrotier (
-        mac VARCHAR(225),
-        last INT(255),
-        power INT(255),
-        label VARCHAR(255)
-    )";
-    if ($conn->query($sql) === TRUE) {
-       echo "Created location table";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
-}
-
 // POST requests from the pi.
 if (isset($_POST['location']) && isset($_POST['label'])) {
     // Create the tables if needed.
@@ -94,12 +64,13 @@ if (isset($_POST['location']) && isset($_POST['label'])) {
 // GET requests from the frontend.
 if (isset($_GET["location"])) {
     $loc = $_GET['location'];
-    $sql = "SELECT name, dsc FROM locations WHERE id='$loc'";
+    $sql = "SELECT name, dsc, avg FROM locations WHERE id='$loc'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $name = $row['name'];
         $dsc = $row['dsc'];
+        $avg = $row['avg'];
     } else {
         $name = "no data";
         $dsc = "no data";
@@ -117,19 +88,20 @@ if (isset($_GET["location"])) {
         $sql = "SELECT * FROM $loc where label='$key'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
+            $points = array();
+            $avgs = array();
             while($row = $result->fetch_assoc()) {
-                $n = array();
-                $points;
-                $avg;
-                for ($i = 0; $i < 48; $i++) {
-                    if ($i < 35)
-                        $points[$i] = random_int(0, 100);
-                    $avg[$i] = random_int(0, 100);
-                }
-                $n['data'] = $points;
-                $n['avg'] = $avg;
-                $data["labels"][$key] = $n;
+                $last = $row['last'];
+                $block = intdiv((time() - intval($last)), (30*60));
+                $points[$block] = $points[$block] + 1;
             }
+            for ($i = 0; $i < 48; $i++) {
+               $avgs[$i] = $avg; 
+            }
+            $n = array();
+            $n['data'] = $points;
+            $n['avg'] = $avgs;
+            $data["labels"][$key] = $n;
         } else {
             $data["labels"][] = "no data";
         }
